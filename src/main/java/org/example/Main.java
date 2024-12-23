3ba71eaa0dc18122b843583bd8d424a5505e2e5f
Ibragimov007 <izafarbek95@gmail.com>
2024-12-23 16:27:11 +0500

    change method calc

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,20 +1,23 @@
 package org.example;
-
+import static java.lang.Integer.parseInt;
 import java.util.Scanner;
 
-import static java.lang.Integer.parseInt;
-
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
+class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
         Scanner objection = new Scanner(System.in);
         System.out.println("Input: ");
 
+        String input = objection.nextLine();
         try {
-            String input = objection.nextLine();
+            String result = calc(input);
+            System.out.println("Result: " + result);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public static String calc(String input) {
+        try {
             String[] parts = input.split(" ");
 
             if(parts.length != 3)
@@ -31,34 +34,25 @@ public class Main {
 
             String operator = parts[1];
 
-            String result = Calculate(a, b, operator);
-            System.out.println("Result: " + result);
-
-        }
-        catch (Exception e)
-        {
-            System.out.println("Error: " + e.getMessage());
-        }
-    }
-
-    private static String Calculate(int a, int b, String operator) {
-        switch (operator) {
-            case "+" -> {
-                return String.valueOf(a + b);
-            }
-            case "-" -> {
-                return String.valueOf(a - b);
-            }
-            case "*" -> {
-                return String.valueOf(a * b);
-            }
-            case "/" -> {
-                if (b == 0) {
-                    throw new ArithmeticException("Деление на ноль не допускается.");
+            switch (operator) {
+                case "+" -> {
+                    return String.valueOf(a + b);
                 }
-                return String.valueOf(a / b);
+                case "-" -> {
+                    return String.valueOf(a - b);
+                }
+                case "*" -> {
+                    return String.valueOf(a * b);
+                }
+                case "/" -> {
+                    return String.valueOf(a / b);
+                }
+                default -> throw new IllegalArgumentException("Incorrect operator: " + operator);
             }
-            default -> throw new IllegalArgumentException("Некорректный оператор: " + operator);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("Incorrect number input.");
+        } catch (Exception e) {
+            throw new IllegalArgumentException("Error: " + e.getMessage());
         }
     }
 }
